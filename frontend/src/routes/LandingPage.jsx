94f8d3c8b68ecdbd296208321c5a53318fea961e
ale_wsl <alejo.ubeto@gmail.com>
2023-05-31 02:43:05 -0600

    [STYLES] landing text distribution

--- frontend/src/routes/LandingPage.jsx
+++ frontend/src/routes/LandingPage.jsx
@@ -8,9 +8,18 @@ export const LandingPage = () => {
   return (
     <section id='Landing'>
       <CustomNavbar/>
-      <div>
-        <h2>landing page</h2>
-        <Link to='/build'><CustomButton text={"Empieza ahora"}/></Link>
+      <section id='landing-01' className='container-cc'>
+        <div className="landing-01-container">
+          <div className='msg-block'>
+            <span>Te damos la bienvenida a</span>
+            <h2>Smart Factory Lab</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos labore inventore quos cumque fugit expedita quas ut ducimus corrupti quisquam a accusamus minima, facere, quibusdam obcaecati odit cupiditate, adipisci quidem?</p>
+            <Link to='/build'><CustomButton text={"Empieza ahora"}/></Link>
+          </div>
+        </div>
+      </section>
+      <div id='img-01'>
+        <img src={require("../assets/images/illustrations/robotics-Illustration_04.png")} alt="img-1" />
       </div>
       <Footer/>
     </section>
