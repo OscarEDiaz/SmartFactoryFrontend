bfae31d95c15c31f90139014164892ee8d0ebcb4
ale_wsl <alejo.ubeto@gmail.com>
2023-06-02 00:14:16 -0600

    [STYLES] minor changes

--- frontend/src/routes/LandingPage.jsx
+++ frontend/src/routes/LandingPage.jsx
@@ -13,10 +13,9 @@ export const LandingPage = () => {
       <section id='landing-01' className='container-cc'>
         <div className="landing-01-container">
           <div className='msg-block'>
-            <span>Te damos la bienvenida a</span>
-            <h2>Smart Factory Lab</h2>
+            <h2>SMART FACTORY LAB</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos labore inventore quos cumque fugit expedita quas ut ducimus corrupti quisquam a accusamus minima, facere, quibusdam obcaecati odit cupiditate, adipisci quidem?</p>
-            <Link to='/build'><CustomButton text={"Empieza ahora"}/></Link>
+            <Link to='/build'><CustomButton text={"PROBAR AHORA"}/></Link>
           </div>
           <div id='img-01'>
             <img src={require("../assets/images/illustrations/robotics-Illustration_04.png")} alt="img-1" />
